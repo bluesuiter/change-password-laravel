9a1105c46d069b887ca2de3107ce65e5c9512875
chauhan1991 <amit.chauhan@ethoswatches.com>
2020-09-07 12:10:47 +0530

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # change-password-laravel
 Change password controller and view
+
+    **Route entry**
+
+    Route::post('/changePassword','HomeController@changePassword')->name('changePassword');
+
+
+In this controller method, We check the following things in order.
+    1. Current password provided by user should match the password stored in the database. We check this by using Hash::check method.
+    2. Current password and the new password should not be same.
+    3. Validate the new password requirements, new password and confirm password should be same.
+
+Once all of this pass through, we can go ahead and change the password for the user account and redirect him back with the success message.
+
--- resources/view/changePassword.blade.php
+++ resources/view/changePassword.blade.php
@@ -1,56 +1,53 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        <form method="POST" action="{{ route('change.password') }}">
-                            @csrf
-
-                            @foreach ($errors->all() as $error)
-                                <p class="text-danger">{{ $error }}</p>
-                            @endforeach
-
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">Current Password</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="current_password" autocomplete="current-password">
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">New Password</label>
-
-                                <div class="col-md-6">
-                                    <input id="new_password" type="password" class="form-control" name="new_password" autocomplete="current-password">
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">New Confirm Password</label>
-
-                                <div class="col-md-6">
-                                    <input id="new_confirm_password" type="password" class="form-control" name="new_confirm_password" autocomplete="current-password">
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Update Password
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+        <div class="row alignForm innerCenter">
+            <div class="col-md-8 mx-auto">
+                <div class="formDesign mx-auto">
+				@if (session('status'))
+					<div class="alert alert-success">
+						{{ session('status') }}
+					</div>
+				@endif
+                @if (session('error'))
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
                     </div>
-                </div>
+                @endif
+				<form method="POST" action="{{ route('change.password') }}">
+					@csrf
+					<div class="form-group">
+						<input id="password" type="password" class="form-control" name="current_password" autocomplete="current-password" placeholder="current password*" required>
+                        @error('current_password')
+                            <span class="invalid-feedback" role="alert">
+								<strong>{{ $message }}</strong>
+							</span>
+                        @enderror
+					</div>
+
+					<div class="form-group">
+						<input id="new_password" type="password" class="form-control" name="new_password" autocomplete="current-password" placeholder="new password*" required>
+                        @error('new_password')
+                            <span class="invalid-feedback" role="alert">
+								<strong>{{ $message }}</strong>
+							</span>
+                        @enderror
+					</div>
+
+					<div class="form-group">
+						<input id="new_password_confirmation" type="password" class="form-control" name="new_password_confirmation" autocomplete="current-password" placeholder="new confirm password">
+                        @error('new_password_confirmation')
+                            <span class="invalid-feedback" role="alert">
+								<strong>{{ $message }}</strong>
+							</span>
+                        @enderror
+					</div>
+
+					<div class="form-group mb-0">
+						<button type="submit" class="cmnBtn w-100">Update Password</button>
+					</div>
+				</form>
+               </div>
             </div>
         </div>
     </div>
